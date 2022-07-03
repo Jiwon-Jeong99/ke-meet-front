9f216b950966dbf06d10f00212043e075082149d
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-07-03 16:47:05 +0900

    설계

--- kemeet-front/src/App.js
+++ kemeet-front/src/App.js
@@ -1,1 +1,13 @@
-import 
+import Nav from './Nav/Nav';
+import SlidingImg from './SlidingImg/SlidingImg';
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <SlidingImg />
+    </div>
+  );
+}
+
+export default App;
--- kemeet-front/src/Home/Nav/Nav.js
+++ kemeet-front/src/Home/Nav/Nav.js
@@ -1,8 +1,8 @@
 
-
 import React from "react";
+import styles from "./Nav.module.css";
 
-const NavDiv = () => {
+const Nav = () => {
   return (
     <div>Navbar</div>
   );
