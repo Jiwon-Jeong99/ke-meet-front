76241b12f75f1bfb2feed5f554389f5b8a353650
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-07-03 17:17:39 +0900

    네브바

--- kemeet-front/src/App.js
+++ kemeet-front/src/App.js
@@ -1,11 +1,15 @@
-import Nav from './Nav/Nav';
-import SlidingImg from './SlidingImg/SlidingImg';
+import Nav from './Home/Nav/Nav';
+import SlidingImg from './Home/SlidingImg/SlidingImg';
+import DateChoice from './Home/DateChoice/DateChoice';
+import PostList from './Home/PostList/PostList';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <Nav />
       <SlidingImg />
+      <DateChoice />
+      <PostList />
     </div>
   );
 }
--- kemeet-front/src/Home/Nav/Nav.js
+++ kemeet-front/src/Home/Nav/Nav.js
@@ -1,11 +1,21 @@
-
 import React from "react";
 import styles from "./Nav.module.css";
 
 const Nav = () => {
   return (
-    <div>Navbar</div>
+    <div className={styles.NavBar}>
+      <img 
+        className={styles.Logo} 
+        alt="logo" 
+        src="../img/logo.png"
+      ></img>
+      <img
+        className={styles.Hamburger}
+        alt="hamburger"
+        src="../img/hamburger.png"
+      ></img>
+    </div>
   );
 };
 
-export default Nav
+export default Nav;
