89248a40a74b0f0691aa34bf77654a34867c53c0
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-07-03 19:21:37 +0900

    네브바

--- kemeet-front/src/App.js
+++ kemeet-front/src/App.js
@@ -1,15 +1,15 @@
 import Nav from './Home/Nav/Nav';
-import SlidingImg from './Home/SlidingImg/SlidingImg';
-import DateChoice from './Home/DateChoice/DateChoice';
-import PostList from './Home/PostList/PostList';
+// import SlidingImg from './Home/SlidingImg/SlidingImg';
+// import DateChoice from './Home/DateChoice/DateChoice';
+// import PostList from './Home/PostList/PostList';
 
 function App() {
   return (
     <div className='App'>
       <Nav />
-      <SlidingImg />
+      {/* <SlidingImg />
       <DateChoice />
-      <PostList />
+      <PostList /> */}
     </div>
   );
 }
